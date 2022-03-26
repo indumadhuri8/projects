45178703cf9fba4a3a21edc30572de09f655b979
indubhargavi504 <madhuri.addanki19@gmail.com>
2022-03-26 14:37:30 +0530

    change

--- com.vogella/src/com/vogella/GitProj.java
+++ com.vogella/src/com/vogella/GitProj.java
@@ -3,7 +3,7 @@ package com.vogella;
 public class GitProj {
 
 	public static void main(String[] args) {
-		System.out.println("hai");
+		System.out.println("hai hello");
 		// TODO Auto-generated method stub
 
 	}
