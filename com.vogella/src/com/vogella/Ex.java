c2b6fd7e41f5e2e8324518480be8c2d7da91f8e2
indubhargavi504 <madhuri.addanki19@gmail.com>
2022-03-26 14:19:34 +0530

    testing program

---
+++ com.vogella/src/com/vogella/Ex.java
@@ -0,0 +1,19 @@
+package com.vogella;
+
+public class Ex {
+	
+
+		public static void main(String[] args) {
+			Ex p = new Ex();
+			System.out.println(p);
+			p=null;
+			System.gc();
+			//System.out.println(p.getEmpid());
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+	// TODO Auto-generated method stub
+		
